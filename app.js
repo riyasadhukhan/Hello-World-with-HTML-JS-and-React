e67e68fe1e124b39ea4b14a8080875df7daff029
Riya <sadhukhanriya55@gmail.com>
2025-07-09 18:52:35 +0530

    Igniting our app

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const innerDiv = React.createElement("div", { id: "reactChild" }, [
   React.createElement("h1", {}, "Hello World from React1!"),
   React.createElement("h2", {}, "Hello World from React2!"),
