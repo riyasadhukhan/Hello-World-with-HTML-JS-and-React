66ca2d01b7cd36e0731c86f00b22c1848576dc70
Riya <sadhukhanriya55@gmail.com>
2025-07-12 11:48:22 +0530

    delivery partner added

--- app.js
+++ app.js
@@ -305,7 +305,7 @@ const Header = () => {
           <img
             className="logo"
             alt="logo"
-            src="https://img.freepik.com/premium-vector/online-shopping-food-delivery-icons-express-delivery-home-set-motorcycle-delivery-service-male-courier-scooter-with-parcel-box_647318-178.jpg"
+            src="https://st3.depositphotos.com/4398873/12574/v/450/depositphotos_125746758-stock-illustration-organic-food-logo.jpg"
           />
           <h2 className="logo-name">DropDine</h2>
         </div>
@@ -373,12 +373,59 @@ const FoodCardList = () => {
   );
 };
 
+const DeliveryPartner = () => {
+  return (
+    <div className="partner-container">
+      <div className="partner-title">
+        <h1 className="partner-header">You can order through apps</h1>
+        <p className="partner-footer">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit enim bibendum
+          sed et aliquet aliquet risus tempor semper.
+        </p>
+      </div>
+      <div className="partner-logo">
+        <img
+          className="partnerimg"
+          src="https://cdn.dribbble.com/userupload/5075239/file/original-cdba4455a5150b370fcea63a99ef8349.jpg"
+          alt="partner-logo"
+        />
+        <img
+          className="partnerimg"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReJIsXLemRHFm9OiPgQgbcfvidzcLu43fdUw&s"
+          alt="partner-logo"
+        />
+        <img
+          className="partnerimg"
+          src="https://play-lh.googleusercontent.com/s0JLCfh27w6kxCd81YnGGZeGIc8KdKx_7d2QEZSlMdXPBYFsN8mAUzW9p1s47QCVGb4"
+          alt="partner-logo"
+        />
+        <img
+          className="partnerimg"
+          src="https://play-lh.googleusercontent.com/MTslvMxGYw8jeXYbWrcGnmYR3OesdO3NzvsYaZ42Vm5O7anRRoDdZst1jSdOOd-JGxc"
+          alt="partner-logo"
+        />
+        <img
+          className="partnerimg"
+          src="https://credyfi.s3.ap-south-1.amazonaws.com/credyfi-1726317867848-zomato-1200x600-1.jpg"
+          alt="partner-logo"
+        />
+        <img
+          className="partnerimg"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRps9FV9h4ZSxrPV3ChB-F_2FREt-cBPIl25Q&s"
+          alt="partner-logo"
+        />
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <>
       <Header />
       <MenuHeroSection />
       <FoodCardList />
+      <DeliveryPartner />
     </>
   );
 };
